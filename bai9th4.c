b8a5671793d84e6a83d1072dd097773f18a43913
rezarria <62886905+rezarria@users.noreply.github.com>
2020-12-30 17:00:37 +0700

    up

--- bai9th4.c
+++ bai9th4.c
@@ -1,109 +1,187 @@
+//205748010310003
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
+#include <string.h> // de dung ham stren
+#include <stdbool.h> // de co kieu bool
 
-typedef struct
+typedef struct HOSO_S
 {
-	char hoten[30], gtinh[4], ngaysinh[10], lop[5];
-}hocsinh;
+	char hoten[30];
+	char gioitinh[10];
+	char lop[10];
+	int ngaysinh[3];
+} HOSO;
 
-typedef hocsinh Mang[100];
-void Nhaptt(hocsinh*a, int *n)
-{
-	int i;
-	printf("So hoc sinh: ");
-	do
-	{
-		scanf("%d", &n);
-		if(*n < 1)
-            printf("\nNhap lai so hoc sinh:  ");
-	} while(n<=0);
-
-	for(i = 0; i < *n; i++)
-	{
-    	fflush(stdin);
-    	printf("\nHoc sinh thu [%d]\n", i+1);
-    	printf("Ho ten : ");
-    	gets(a[i].hoten);
-    	
-    	fflush(stdin);
-    	printf("Ngay sinh: ");
-  		gets(a[i].ngaysinh);
-        
-        fflush(stdin);
-        printf("Gioi tinh: ");
-        gets(a[i].gtinh);
-        
-        fflush(stdin);
-        printf("Lop: ");
-        gets(a[i].lop);
-    }
-}
-
-void tachten(char hoten[],char ten[])
-{
-	for(int i=strlen(hoten)-1;i>=0;i--)
-	{
-		if(hoten[i]==' ')
-		{
-			strcpy(ten,hoten+1+i);
-			break;
-		}
-	}
-}
-
-void Sx(hocsinh*a, int n)
-{
-	for(int i=0; i<n; i++)
-	{
-		for(int j=i+1; j< n; j++)
-		{
-			char ten1[30] = "", ten2[30] = "";
-			tachten(a[i].hoten,ten1);
-			tachten(a[j].hoten,ten2);
-			int check1=strcmpi(ten1,ten2);
-				if(check1>0)
-				{
-					hocsinh tam = a[i];
-					a[i] = a[j];
-					a[j] = tam;
-				}
-				else if(check1==0)
-				{
-					int check2=strcmpi(a[i].hoten,a[j].hoten);
-						if(check2>0)
-						{
-							hocsinh tam = a[i];
-							a[i] = a[j];
-							a[i] = tam;
-						}
-				}
-		}
-	}
-}
-
-void hien(hocsinh*a, int n)
-{
-	int i;
-	printf("\n-----------------------------");
-	printf("\nDanh sach thi sinh");
-	printf("\tHo ten: %-21s", a[i].hoten);
-	printf("\tGioi tinh: %-s", a[i].gtinh);
-	printf("\tNgay sinh: %-s", a[i].ngaysinh);
-	printf("\tLop: %-s", a[i].lop);
-	printf("\n-----------------------------");
-}
+void nhapSo(int* n);
+char* tachTen(char* hoten);
+bool soSanhTen(char* a, char* b);
+void nhapHoSo(HOSO hoso[], int n);
+void hienHoSo(HOSO hoso[], int n);
+void xepHoSo(HOSO hoso[], int n);
+void traoHoSo(HOSO* a, HOSO* b);
 
 int main()
 {
-	Mang a;
-	int n,i,j;
-	
-	Nhaptt(a, &n);
-	tachten(hoten, ten);
-	Sx(a,n);
-	hien(a,n);
-	
-	getch;
+	int n;
+
+	nhapSo(&n);
+
+	HOSO hoso[n];
+
+	nhapHoSo(hoso, n);
+
+	xepHoSo(hoso, n);
+
+	hienHoSo(hoso, n);
+
+
 	return 0;
+}
+
+void nhapSo(int* n)
+{
+	do
+	{
+		printf("So hoc sinh : ");
+		scanf("%d", n);
+		if (*n < 1)
+			printf("Sai pham vi!!!");
+	} while (*n < 1); // neu so be hon 1 thi nhap lai
+}
+
+void hienHoSo(HOSO hoso[], int n)
+{
+	/*
+		hoten : 30 ki tu -> %-30s
+		lop : 10 ki tu -> %-10s
+		gioi tinh : 10 ki tu ->%-10s
+		ngay sinh 2 + 2 + 4 + 3 ki tu ':' = 11 ki tu ->%-11s
+	*/
+	printf("|--------------------------------------------------------------------------|\n");
+	printf("|%-30s\t|%-10s\t|%-10s\t|%-10s|\n", "Ho va ten", "Lop", "Gioi tinh", "Ngay sinh");
+	printf("|--------------------------------------------------------------------------|\n");
+	
+	int i;
+	for (i = 0; i < n; i++)
+		printf("|%-30s\t|%-10s\t|%-10s\t|%02d/%02d/%04d|\n", hoso[i].hoten, hoso[i].lop, hoso[i].gioitinh, hoso[i].ngaysinh[0], hoso[i].ngaysinh[1], hoso[i].ngaysinh[2]);
+	printf("|--------------------------------------------------------------------------|\n");
+}
+
+void nhapHoSo(HOSO hoso[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf("----------------------------------------\n");
+
+		printf("Ho va ten\t: ");
+		fflush(stdin);
+		gets(hoso[i].hoten);
+
+		printf("Gioi tinh\t: ");
+		fflush(stdin);
+		gets(hoso[i].gioitinh);
+
+		printf("Ngay sinh\t: ");
+		// Cach nhap ngay sinh : nhap SO + cach + nhap SO + cach + nhap SO + enter
+		scanf("%d%d%d", &hoso[i].ngaysinh[0], &hoso[i].ngaysinh[1], &hoso[i].ngaysinh[2]);
+
+		printf("Lop\t\t: ");
+		fflush(stdin);
+		gets(hoso[i].lop);
+	}
+}
+
+void xepHoSo(HOSO hoso[], int n)
+{
+	int i, j;
+	/*
+		tachTen --> soSanh
+		nen soSanh(  tachTen(HoTen_1)-->Ten_1  ,  tachTen(HoTen-2)-->Ten_2   )
+	*/
+	for (i = 0; i < n - 1; i++)
+		for (j = i + 1; j < n; j++)
+			if (soSanhTen(tachTen(hoso[i].hoten), tachTen(hoso[j].hoten)))
+				traoHoSo(&hoso[i], &hoso[j]);
+}
+
+void traoHoSo(HOSO* a, HOSO* b)
+{
+	HOSO t = *a;
+	*a = *b;
+	*b = t;
+}
+
+char* tachTen(char* hoten)
+{
+	/*
+		vi tri cua hien tai
+		hoten -
+			  |
+			  V
+			  vo ta nam
+
+		ta can dua vi tri ve cuoi
+		hoten += khoang cach tu vi tri dau den vi tri cuoi
+		dung ham strlen de tinh khoang cach
+
+		hoten ---------
+					  |
+					  V
+			  vo ta nam
+	*/
+	hoten += strlen(hoten);
+
+	/*
+		gia can tim dia chi BAT DAU cua TEN
+		logic co ban -> chac chan co dau cach truoc phan TEN
+		Ta tim bang cach
+			di lui tu vi tri cuoi
+			gap chu ' ' tuc dau cach thi dung lai
+	*/
+	while (*hoten != ' ')
+		hoten--;
+
+	// thu ta can la vi tri BAT DAU cua ten, khong phai cua chu ' '
+	// nen + 1 de sua lai vi tri
+	// tra dia chi ve
+	return hoten + 1;
+}
+
+bool soSanhTen(char* a, char* b)
+{
+	bool kq = true;
+	int i;
+	/*
+		ta can co chieu dai ngan nhat trong 2 xau
+		neu a be hon thi lay cua a
+		neu b be hon thi lay cua b
+	*/
+	int length = strlen(a) < strlen(b) ? strlen(a) : strlen(b);
+
+	/*
+		tai sao?
+		vi du: nam va duong
+		nam -> 3 ki tu
+		duong -> 5 ki tu
+		khi so sanh tung ki tu mot
+		ta KHONG THE lay ki tu thu 4 cua nam duoc
+		vi no vo li, nam chi co 3 ki tu
+	*/
+
+
+	/*
+		so sanh tung ki tu mot, trai -> phai
+		neu co khac nhau thi HUY vong lap
+		kq se la noi luu ket qua so sanh
+	*/
+	for (i = 0; i < length; i++)
+		if (a[i] != b[i])
+		{
+			kq = a[i] < b[i];
+			// NEU ki tu ben a xuat hien truoc --> TRUE
+			// NEU ki tu ben a xuat hien SAU --> FALSE
+			break; // HUY vong lap
+		}
+	//NEU MUON DAO NGUOC THI THEM ! truoc kq
+	return kq;
 }
